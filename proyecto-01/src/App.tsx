e91d3425bb7a896f561935f8ebaa28a634c53ce1
Robert <r0b3rt0gzz@gmail.com>
2025-12-11 15:30:15 -0300

    Separacion de componentes, y responsive

--- proyecto-01/src/App.tsx
+++ proyecto-01/src/App.tsx
@@ -1,20 +1,12 @@
 import Home from "./components/Home"
-import ShowCurrentLinkElement from "./components/ShowLiks"
-import SwitchButtons from "./special/SwitchButtons"
+import ShowGallery from "./components/Gallery"
 
 function App() {
   return (
     <>
-    <div className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-5 pt-5 h-screen w-full bg-gray-950 border-gray-600">
+    <div className="flex flex-col xl:flex-row items-center xl:items-start justify-center gap-5 pt-5 xl:h-screen bg-gray-950 border-gray-600">
       <Home />
-
-      <div className="flex flex-col gap-y-5 pt-2 items-center w-200 lg:w-225">
-        <SwitchButtons />
-        <div className="flex flex-col gap-y-5 p-5 bg-gray-900 border-2 rounded-4xl border-gray-800">
-          <ShowCurrentLinkElement />
-          <iframe id="iframeVisor" className="border-2 border-gray-800 rounded-2xl h-[70vh] w-[90vw] lg:w-210 shadow-[0_4px_6px_rgba(0,0,0,0.5)]" src="/src/assets/DemoElements.html"></iframe>
-        </div>
-      </div>
+      <ShowGallery />
     </div>
     </>
   )
