0c52cca6bf25cb52fa087deb07aed8d317e45958
Robert <r0b3rt0gzz@gmail.com>
2026-03-01 00:57:45 -0300

    Mejoras en el responsive, parrafos y ahora los estilos se sirven desde jsdeliver del paquete npm, la pagina es una muestra del funcionamiento de la etiqueta link. Elimine los archivos css de los que dependia antes. Pagina lista para deploy

--- proyecto-01/src/components/Home.tsx
+++ proyecto-01/src/components/Home.tsx
@@ -6,29 +6,29 @@ function Hero(){
         
     return (
         <header className="px-3 md:pl-5 md:pr-0 xl:w-170">
-            <h1 className="mb-2">CSS Switch Gallery</h1>
-            <h2 className="mb-5">Una <strong>colección de estilos CSS listos para usar</strong> en cualquier proyecto!</h2>
+            <h1 className="mb-2" translate="no">CSS Switch Gallery</h1>
+            <h2 className="mb-5">Una <strong>colección de estilos CSS listos para usar</strong> y que darán vida a tu web!</h2>
 
             <div className="mb-2 *:mb-3">
                 <p>
-                    Navega entre diferentes estilos usando los <code className="py-1 px-2">botones switch</code> sobre el visualizador de elementos html. En el verás como lucirán en tu propia web!
+                    Navega entre diferentes estilos usando los <code className=" text-nowrap">botones switch</code> sobre el visualizador de elementos html. En el verás como lucirán tus elementos HTML!
                 </p>
                 <p>
                     También puedes alternar entre <span>modo oscuro y claro</span>. Las plantillas incluyen algunas clases para uso rápido y manual.
                 </p>
                 <p>
-                    Sabrás sobre las clases scrolleando en el visualizador.
+                    Sabrás más sobre las clases desplazándote por el visualizador.
                 </p>
                 <p>
-                    Para aplicar el css en tu proyecto, copia el elemento <code className="py-1 px-2">{'<link/>'}</code> sobre el visualizador y pégalo en el <code className="py-1 px-2">{'<head/>'}</code> de tu html mediante le etiqueta link.
+                    Para aplicar el CSS en tu proyecto, copia el elemento <code>{'<link/>'}</code> que se encuentra sobre el visualizador y pégalo en el <code>{'<head/>'}</code> de tu HTML.
                 </p>
                 <p>
-                    También puedes <a href={cssFilesRoutes[currentCss]} target="_blank" rel="noopener noreferrer">descargar la plantilla</a> y editarla manualmente.
+                    También puedes <a href={cssFilesRoutes[currentCss]} target="_blank" rel="noopener noreferrer">descargar la plantilla aquí</a> y editarla manualmente.
                 </p>
             </div>
 
-            <a href="https://github.com/RobertGmzz/CSS-Styles-Gallery/tree/master/proyecto-01" target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 py-1 px-2 w-auto">
-                <img src="./github.svg" alt="github icon"/>project</a>
+            <a href="https://github.com/RobertGmzz/CSS-Styles-Gallery/tree/master/proyecto-01" target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 w-auto">
+                <img src="./github.svg" alt="github icon"/>proyecto</a>
         </header>
     )
 }
