485cbf2a806278fa438a995326be2bf7a680aaae
Robert <r0b3rt0gzz@gmail.com>
2026-03-02 19:24:12 -0300

    minima correccion en responsive

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -6,7 +6,7 @@ function ShowGallery(){
     return (
         <section className="flex flex-col items-center gap-y-5 my-2 sm:px-2">
             <MagicButtons />
-            <div className="preview flex flex-col gap-y-5 p-3 md:p-5">
+            <div className="preview flex flex-col items-center gap-y-5 p-3 md:p-5">
                 <ShowCurrentLinkElement />
                 <Iframe />
             </div>
